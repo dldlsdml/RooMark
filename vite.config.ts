043ca561a2a2e87d02922509a864fa149b9ba42a
Seoyun <iseoyun750@gmail.com>
2026-04-18 19:24:00 +0900

    chore: docs/index.html 및 빌드 환경 최신화

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    viteSingleFile({
-      useRecommendedBuildConfig: true,
-      removeViteModuleLoader: true,
-      inlinePattern: ['**/*'],
-      deleteInlinedFiles: true,
-    }),
+    viteSingleFile()
   ],
   build: {
-    outDir: 'docs',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: 'index.html',
-      output: {
-        entryFileNames: 'index.html',
-      },
-    },
-    assetsInlineLimit: 100000000,
-  },
+    outDir: 'docs'
+  }
 })
